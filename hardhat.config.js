c404bad5b4950b1f052d48b10906f06cd335fc8a
dveth2 <klee78736@gmail.com>
2022-10-24 07:28:16 -0500

    update hardhat config, add env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
