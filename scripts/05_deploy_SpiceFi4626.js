6fdf2287285e67098d4f9c4a94061a8749b569a4
dveth2 <klee78736@gmail.com>
2022-12-11 21:11:55 -0500

    new deployments, minor deployment script edits

--- scripts/05_deploy_SpiceFi4626.js
+++ scripts/05_deploy_SpiceFi4626.js
@@ -9,7 +9,7 @@ async function main() {
     hre.network.name === "mainnet"
       ? "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" // mainnet weth
       : "0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6"; // goerli weth
-  const args = [WETH, deployer.address, deployer.address, 200];
+  const args = [WETH, deployer.address, deployer.address, 0];
 
   const SpiceFi4626 = await ethers.getContractFactory("SpiceFi4626");
   const vault = await upgrades.deployProxy(SpiceFi4626, args, {
