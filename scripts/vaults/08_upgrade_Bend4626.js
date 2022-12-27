f797b60682c22d8ea89447598b1245ce6b7517f6
dveth2 <klee78736@gmail.com>
2022-12-26 20:04:07 -0600

    verify contracts after upgrade

--- scripts/vaults/08_upgrade_Bend4626.js
+++ scripts/vaults/08_upgrade_Bend4626.js
@@ -5,9 +5,20 @@ async function main() {
 
   const vaultAddress = "0x9f89770af1da5df1b1cbf09b8ee954a2c29b0259";
   const Bend4626 = await ethers.getContractFactory("Bend4626");
+  const impl = await upgrades.deployImplementation(Bend4626);
   await upgrades.upgradeProxy(vaultAddress, Bend4626);
 
   console.log("Bend4626 successfully upgraded!");
+
+  if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
+    try {
+      await hre.run("verify:verify", {
+        address: impl,
+        contract: "contracts/vaults/Bend4626.sol:Bend4626",
+        constructorArguments: [],
+      });
+    } catch (_) {}
+  }
 }
 
 main().catch((error) => {
--- scripts/vaults/09_upgrade_Drops4626.js
+++ scripts/vaults/09_upgrade_Drops4626.js
@@ -5,9 +5,20 @@ async function main() {
 
   const vaultAddress = "0x8e44109d4979f6fa961fc27ffb8452360fc0b5b4";
   const Drops4626 = await ethers.getContractFactory("Drops4626");
+  const impl = await upgrades.deployImplementation(Drops4626);
   await upgrades.upgradeProxy(vaultAddress, Drops4626);
 
   console.log("Drops4626 successfully upgraded!");
+
+  if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
+    try {
+      await hre.run("verify:verify", {
+        address: impl,
+        contract: "contracts/vaults/Drops4626.sol:Drops4626",
+        constructorArguments: [],
+      });
+    } catch (_) {}
+  }
 }
 
 main().catch((error) => {
