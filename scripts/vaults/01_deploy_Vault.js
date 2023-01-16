591ef10737448b64ce9fa8bbf5fcb21d36902eed
dveth2 <klee78736@gmail.com>
2023-01-15 19:13:54 -0600

    use beacon proxy for Vault

--- scripts/vaults/01_deploy_Vault.js
+++ scripts/vaults/01_deploy_Vault.js
@@ -1,16 +1,31 @@
 const hre = require("hardhat");
+const constants = require("../../test/constants");
 
 async function main() {
-  const { ethers, upgrades } = hre;
+  const { ethers, upgrades, deployments } = hre;
 
   const WETH =
     hre.network.name === "mainnet"
       ? "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" // mainnet weth
       : "0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6"; // goerli weth
-  const args = ["Spice Peer2Peer", "SPP", WETH, 0];
+  const args = [
+    "Spice Peer2Peer",            // name
+    "SPP",                        // symbol
+    WETH,                         // asset
+    [],                           // marketplaces
+    constants.accounts.Dev,       // creator
+    constants.accounts.Dev,       // dev
+    constants.accounts.Multisig,  // multisig
+    constants.accounts.Multisig,  // fee recipient
+  ];
 
   const Vault = await ethers.getContractFactory("Vault");
-  const vault = await upgrades.deployProxy(Vault, args, { kind: "uups" });
+  const beacon = await upgrades.deployBeacon(Vault);
+  await beacon.deployed();
+
+  await deployments.save("Vault", beacon);
+
+  const vault = await upgrades.deployBeaconProxy(beacon, Vault, args);
 
   await vault.deployed();
 
