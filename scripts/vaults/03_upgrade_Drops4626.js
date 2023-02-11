d74d979107734e22edf8d8a5b8a955fb4f194d0d
dveth2 <klee78736@gmail.com>
2023-02-11 13:41:56 -0500

    upgrade drops

--- scripts/vaults/03_upgrade_Drops4626.js
+++ scripts/vaults/03_upgrade_Drops4626.js
@@ -3,17 +3,17 @@ const hre = require("hardhat");
 async function main() {
   const { ethers, upgrades } = hre;
 
-  const beacon = await deployments.get("Drops4626");
-  const Drops4626 = await ethers.getContractFactory("Drops4626");
-  const vault = await upgrades.upgradeBeacon(beacon.address, Drops4626, {'timeout': 0});
-  await vault.deployed();
-
-  console.log("Drops4626 successfully upgraded!");
+  // const beacon = await deployments.get("Drops4626");
+  // const Drops4626 = await ethers.getContractFactory("Drops4626");
+  // const vault = await upgrades.upgradeBeacon(beacon.address, Drops4626, {'timeout': 0});
+  // await vault.deployed();
+// 
+  // console.log(`Drops4626 successfully upgraded to ${vault.address}!`);
 
   if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
     try {
       await hre.run("verify:verify", {
-        address: vault.address,
+        address: "0xDFf540f3922d53544aEBa4D0Aec0f0c42c706904",
         contract: "contracts/vaults/Drops4626.sol:Drops4626",
         constructorArguments: [],
       });
