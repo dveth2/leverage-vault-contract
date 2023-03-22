6924f8c0336b5c38f9562f751d8c5c35545aae31
dveth2 <klee78736@gmail.com>
2023-03-22 16:44:49 -0400

    upgrade escrow contract (withdrawal)

--- scripts/lending/upgrade.js
+++ scripts/lending/upgrade.js
@@ -3,18 +3,18 @@ const hre = require("hardhat");
 async function main() {
   const { ethers, upgrades } = hre;
 
-  const beacon = await deployments.get("SpiceLending");
-  const SpiceLending = await ethers.getContractFactory("SpiceLending");
-  const vault = await upgrades.upgradeBeacon(beacon.address, SpiceLending);
-  await vault.deployed();
-
-  console.log("SpiceLending successfully upgraded!");
+  // const beacon = await deployments.get("SpiceLending");
+  // const SpiceLending = await ethers.getContractFactory("SpiceLending");
+  // const vault = await upgrades.upgradeBeacon(beacon.address, SpiceLending);
+  // await vault.deployed();
+// 
+  // console.log("SpiceLending successfully upgraded!");
 
   if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
     try {
       await hre.run("verify:verify", {
-        address: vault.address,
-        contract: "contracts/vaults/SpiceLending.sol:SpiceLending",
+        address: '0x2136547312e13bd09Ee94321C5145C0661Cd0a48',
+        contract: "contracts/lending/SpiceLending.sol:SpiceLending",
         constructorArguments: [],
       });
     } catch (_) {}
