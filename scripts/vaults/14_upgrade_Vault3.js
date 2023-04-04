f4de563d36597cda372785ea99a105ce69fff723
dveth2 <klee78736@gmail.com>
2023-04-04 08:06:42 -0400

    vault upgrade on goerli

--- scripts/vaults/14_upgrade_Vault3.js
+++ scripts/vaults/14_upgrade_Vault3.js
@@ -3,20 +3,19 @@ const hre = require("hardhat");
 async function main() {
   const { ethers, upgrades, deployments } = hre;
 
-  // const beacon = await deployments.get("Vault3");
-  // const Vault = await ethers.getContractFactory("Vault");
-  // // await upgrades.forceImport("0xd7650014dBdB486154c1E86F73CC750cc280b6E3", Vault);
-  // const vault = await upgrades.upgradeBeacon(beacon.address, Vault);
-  // await vault.deployed();
-// 
-  // console.log("Vault successfully upgraded!");
-// 
-  // const implAddr = await beacon.implementation();
+  const beacon = await deployments.get("Vault3");
+  const Vault = await ethers.getContractFactory("Vault");
+  const vault = await upgrades.upgradeBeacon(beacon.address, Vault);
+  await vault.deployed();
+
+  console.log("Vault successfully upgraded!");
+
+  const implAddr = await beacon.implementation();
 
   if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
     try {
       await hre.run("verify:verify", {
-        address: "0xd98BC057CF3020A9911cEA0233d3b372DC8fB539",
+        address: implAddr,
         contract: "contracts/vaults/Vault.sol:Vault",
         constructorArguments: [],
       });
