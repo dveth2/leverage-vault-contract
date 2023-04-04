9003579b97cb3a1f6e1d5b434ce2f71c39d3cca1
dveth2 <klee78736@gmail.com>
2023-04-04 09:40:14 -0400

    deploy mainnet vault tracking loans

--- scripts/adapters/02_deploy_NFTfi.js
+++ scripts/adapters/02_deploy_NFTfi.js
@@ -6,18 +6,18 @@ async function main() {
   const args = [
     "0x0C90C8B4aa8549656851964d5fB787F0e4F54082", // DirectLoanCoordinator
   ];
-  const NFTfiNoteAdapterFactory = await ethers.getContractFactory(
-    "NFTfiNoteAdapter"
-  );
-  const adapter = await NFTfiNoteAdapterFactory.deploy(...args);
-  await adapter.deployed();
-
-  console.log(`NFTfi Note Adapter deployed to ${adapter.address}`);
+  // const NFTfiNoteAdapterFactory = await ethers.getContractFactory(
+    // "NFTfiNoteAdapter"
+  // );
+  // const adapter = await NFTfiNoteAdapterFactory.deploy(...args);
+  // await adapter.deployed();
+  // await deployments.save("NftfiNoteAdapter", adapter);
+  // console.log(`NFTfi Note Adapter deployed to ${adapter.address}`);
 
   if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
     try {
       await hre.run("verify:verify", {
-        address: adapter.address,
+        address: "0x03e8254a18DD8F44C2606813dAA70e5cb5059D15",
         contract:
           "contracts/integrations/NFTfi/NFTfiNoteAdapter.sol:NFTfiNoteAdapter",
         constructorArguments: args,
