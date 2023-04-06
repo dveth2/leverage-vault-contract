072418453570084d1be6e848d01386b43553d573
dveth2 <klee78736@gmail.com>
2023-04-06 09:36:00 -0400

    upgrade para mainnet

--- scripts/vaults/11_upgrade_Para4626.js
+++ scripts/vaults/11_upgrade_Para4626.js
@@ -3,17 +3,17 @@ const hre = require("hardhat");
 async function main() {
   const { ethers, upgrades } = hre;
 
-  const beacon = await deployments.get("Para4626");
-  const Para4626 = await ethers.getContractFactory("Para4626");
-  const vault = await upgrades.upgradeBeacon(beacon.address, Para4626);
-  await vault.deployed();
-
-  console.log("Para4626 successfully upgraded!");
+  // const beacon = await deployments.get("Para4626");
+  // const Para4626 = await ethers.getContractFactory("Para4626");
+  // const vault = await upgrades.upgradeBeacon(beacon.address, Para4626);
+  // await vault.deployed();
+// 
+  // console.log("Para4626 successfully upgraded!");
 
   if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
     try {
       await hre.run("verify:verify", {
-        address: vault.address,
+        address: "0x968D4f25997095e33D5C115Fa2c7AE273f2c0d4C",
         contract: "contracts/vaults/Para4626.sol:Para4626",
         constructorArguments: [],
       });
