7326aff455d6fd44c2ac24cf4901af422655afe7
dveth2 <klee78736@gmail.com>
2023-10-07 16:05:49 +0200

    vault upgrades goerli and mainnet

--- scripts/vaults/16_upgrade_Vault.js
+++ scripts/vaults/16_upgrade_Vault.js
@@ -1,19 +1,20 @@
+const { ValidationsCacheOutdated } = require("@openzeppelin/hardhat-upgrades/dist/utils");
 const hre = require("hardhat");
 
 async function main() {
   const { ethers, upgrades, deployments } = hre;
 
-  const beacon = await deployments.get("VaultBeacon");
-  const Vault = await ethers.getContractFactory("Vault");
-  const vault = await upgrades.upgradeBeacon(beacon.address, Vault);
-  await vault.deployed();
-
-  console.log("Vault Implementation successfully upgraded!");
+  // const beacon = await deployments.get("VaultBeacon");
+  // const Vault = await ethers.getContractFactory("Vault");
+  // const vault = await upgrades.upgradeBeacon(beacon.address, Vault);
+  // await vault.deployed();
+// 
+  // console.log("Vault Implementation successfully upgraded!");
 
   if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
     try {
       await hre.run("verify:verify", {
-        address: vault.address,
+        address: "0xA53F094EF1978f60EF2cca51adfFE139beE97bcA",
         contract: "contracts/vaults/Vault.sol:Vault",
         constructorArguments: [],
       });
