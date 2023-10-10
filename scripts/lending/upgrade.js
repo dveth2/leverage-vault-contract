68ff7ac38002b5e78e4d7d17906a978eaee179f1
dveth2 <klee78736@gmail.com>
2023-10-10 15:59:57 +0200

    upgrade old escrow on goerli

--- scripts/lending/upgrade.js
+++ scripts/lending/upgrade.js
@@ -4,16 +4,16 @@ async function main() {
   const { ethers, upgrades } = hre;
 
   // const beacon = await deployments.get("SpiceLending");
-  // const SpiceLending = await ethers.getContractFactory("SpiceLending");
-  // const vault = await upgrades.upgradeBeacon(beacon.address, SpiceLending);
-  // await vault.deployed();
-// 
-  // console.log("SpiceLending successfully upgraded!");
+  const SpiceLending = await ethers.getContractFactory("SpiceLending");
+  const vault = await upgrades.upgradeBeacon("0x44Caf1C51A8Db5f9F8057A11560e7d66E50635D6", SpiceLending);
+  await vault.deployed();
+
+  console.log("SpiceLending successfully upgraded!");
 
   if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
     try {
       await hre.run("verify:verify", {
-        address: "0xFE4bF631B64b080b825436831e5294926DF785AB",
+        address: vault.address,
         contract: "contracts/lending/SpiceLending.sol:SpiceLending",
         constructorArguments: [],
       });
