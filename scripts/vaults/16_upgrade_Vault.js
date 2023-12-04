2bf09482f309f40d417af8919164ed740b80f35a
dveth2 <klee78736@gmail.com>
2023-12-05 00:53:31 +0100

    upgrade vault mainnet

--- scripts/vaults/16_upgrade_Vault.js
+++ scripts/vaults/16_upgrade_Vault.js
@@ -4,7 +4,7 @@ const hre = require("hardhat");
 async function main() {
   const { ethers, upgrades, deployments } = hre;
 
-  const beacon = await deployments.get("VaultBeacon");
+  const beacon = await deployments.get("Vault");
   const Vault = await ethers.getContractFactory("Vault");
   const vault = await upgrades.upgradeBeacon(beacon.address, Vault);
   await vault.deployed();
@@ -14,7 +14,7 @@ async function main() {
   if (hre.network.name !== "localhost" && hre.network.name !== "hardhat") {
     try {
       await hre.run("verify:verify", {
-        address: "",
+        address: vault.address,
         contract: "contracts/vaults/Vault.sol:Vault",
         constructorArguments: [],
       });
